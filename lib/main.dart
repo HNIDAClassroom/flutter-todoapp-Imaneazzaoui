871a389481244c97779171a176ed726c3b7ee95d
Imaneazzaoui <azzaouiimane1@gmail.com>
2023-10-13 16:04:36 +0100

    models

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:todolist_app/tasks.dart';
+
+
 void main() {
   runApp(
     const MaterialApp(
-    //  home: ...,
+      home: Tasks(),
     ),
   );
 }
